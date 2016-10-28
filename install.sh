e0f05873cc666cfb7a0c0ffd934aa77d375c7fc7
Jente <jente.adams@gmail.com>
2016-10-28 18:18:59 +0200

    changed script fixed errors and added sudo where needed

--- install.sh
+++ install.sh
@@ -2,15 +2,29 @@
 
 #install i3 wm and extra packages to make config work
 function i3 {
-    pacaur -S i3-wm i3lock i3blocks xtitle feh compton rofi --noconfirm
+    pacaur -S i3-wm i3lock i3blocks xtitle feh compton rofi
     cp i3/config i3blocks.conf ~/.config/i3/
     cp i3/lockscreen.png i3/wallpaper.jpg ~/Afbeeldingen/
-    cp -rf i3/i3blocks /usr/lib/
+    sudo cp -rf i3/i3blocks /usr/lib/
     cd ~/Downloads
     git clone https://github.com/FortAwesome/Font-Awesome.git
-    cp Font-Awesome/fonts)*.ttf /usr/share/fonts/TTF/
+    sudo p Font-Awesome/fonts/*.ttf /usr/share/fonts/TTF/
     rm -rf Font-Awesome
     git clone https://github.com/supermarin/YosemiteSanFranciscoFont.git
-    cp YosemiteSanFranciscoFont/*.ttf /usr/share/fonts/TTF/
+    sudo cp YosemiteSanFranciscoFont/*.ttf /usr/share/fonts/TTF/
     rm -rf YosemiteSanFranciscoFont
 }
+
+#function pacaur {
+
+#}
+
+#function lightdm {
+
+#}
+
+#function st {
+
+#}
+
+i3
